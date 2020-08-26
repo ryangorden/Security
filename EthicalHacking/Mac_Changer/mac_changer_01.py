c38973fc3f8813434ce6fb2046a6258dfdb6468a
Ryan Gorden <ryangorden@gmail.com>
2020-08-26 01:38:51 -0400

    put config changes in a function

--- EthicalHacking/Mac_Changer/mac_changer_01.py
+++ EthicalHacking/Mac_Changer/mac_changer_01.py
@@ -9,7 +9,13 @@ import sys
 interface= sys.argv[1]
 mac_address= sys.argv[2]
 
-print('Changing the MAC Address for', interface, 'to', mac_address)
-subprocess.call(['ip', 'link', 'set', interface,'down']) #ifcong eth0 down can be used as well
-subprocess.call(['ifconfig', interface, 'hw', 'ether', mac_address])
-subprocess.call(['ip', 'link', 'set', interface,'up'])
+def change_mac(interface, mac_address):
+    print('Changing the MAC Address for', interface, 'to', mac_address)
+    subprocess.call(['ip', 'link', 'set', interface,'down']) #ifcong eth0 down can be used as well
+    subprocess.call(['ifconfig', interface, 'hw', 'ether', mac_address])
+    subprocess.call(['ip', 'link', 'set', interface,'up'])
+    return 'Mac Address has been changed to ' + mac_address
+
+if __name__ == '__main__':
+    notification= change_mac(interface, mac_address)
+    print(notification)
