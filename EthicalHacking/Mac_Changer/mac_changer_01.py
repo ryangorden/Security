04b38fc598ad5b360e5d6472b153dcc77b1c10b7
Ryan Gorden <ryangorden@gmail.com>
2020-08-26 14:21:45 -0400

    Made program compatible with Python 2 and 3

--- EthicalHacking/Mac_Changer/mac_changer_01.py
+++ EthicalHacking/Mac_Changer/mac_changer_01.py
@@ -24,11 +24,19 @@ def validate_mac_change(arguments):
     print('Validating the Mac address has changed to '+ arguments['mac_address'])
     interface= subprocess.check_output(['ip', 'link', 'show', arguments['interface']])
     interfacetext= interface
-    mactxt= re.search(r'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w', interfacetext)
-    if mactxt.group(0)== arguments['mac_address']:
-        print('Mac Address has been changed to ' + arguments['mac_address'])
-    else:
-        print('The mac address was not changed.')
+    try:
+        mactxt= re.search(r'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w', interfacetext)
+        if mactxt.group(0)== arguments['mac_address']:
+            print('Mac Address has been changed to ' + arguments['mac_address'])
+        else:
+            print('The mac address was not changed.')
+    except:
+        mactxt= re.search(r'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w', interfacetext.decode('utf8'))
+        if mactxt.group(0)== arguments['mac_address']:
+            print('Mac Address has been changed to ' + arguments['mac_address'])
+        else:
+            print('The mac address was not changed.')
+
 
 
 if __name__ == '__main__':
